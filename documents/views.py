838e9ff173392ad58627e27144c38e89d954666f
Alejandro Hdez <alherdom@outlook.com>
2024-01-17 19:01:56 +0000

    Fixed: all test ut4 errors.

--- documents/views.py
+++ documents/views.py
@@ -7,24 +7,29 @@ from .forms import EditDocumentForm
 
 
 def home(request):
-    new_doc = Document.objects.create()
-    return redirect("documents:edit_document", docref=new_doc.ref)
+    # return redirect("documents:edit_document", docref=new_doc.ref)
+    return redirect(Document.objects.create())
 
-@require_http_methods(['GET', 'POST'])
+
+@require_http_methods(["GET", "POST"])
 def edit_document(request, docref):
     doc = get_object_or_404(Document, ref=docref)
-    form = EditDocumentForm(request.POST or None, instance=doc)
+    form = EditDocumentForm(request.POST, instance=doc)
     if request.method == "POST":
         if form.is_valid():
             doc = form.save()
             messages.success(request, "Document was successfully saved!")
         else:
             messages.error(request, "There are errors in form!")
-        return render(request, "documents/edit.html", {"form":form, "doc":doc} )
-    return render(request, "documents/edit.html", {"form":form, "doc":doc})
+        return render(request, "documents/edit.html", {"form": form, "doc": doc})
+    return render(request, "documents/edit.html", {"form": form, "doc": doc})
 
 
 def render_document(request, docref):
     doc = get_object_or_404(Document, ref=docref)
     markdown_contents = markdown.markdown(doc.contents)
-    return render(request, "documents/render.html", {"doc":doc, "markdown_contents":markdown_contents})
+    return render(
+        request,
+        "documents/render.html",
+        {"doc": doc, "markdown_contents": markdown_contents},
+    )
